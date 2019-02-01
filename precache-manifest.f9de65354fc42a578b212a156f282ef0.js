2f1e61861e351ebba331fccd777b3d7a7d713980
Michael Simms <drsid1123@gmail.com>
2019-02-01 01:46:28 -0500

    Updates

---
+++ precache-manifest.f9de65354fc42a578b212a156f282ef0.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "cddd0653cc71d6b754824c9ba6d9615a",
+    "url": "/epidemic/static/media/contingency.cddd0653.png"
+  },
+  {
+    "revision": "9cdc425c5f293afc85d4",
+    "url": "/epidemic/static/css/main.f4169998.chunk.css"
+  },
+  {
+    "revision": "fa9ebe0057800cfdd8ab",
+    "url": "/epidemic/static/js/1.fa9ebe00.chunk.js"
+  },
+  {
+    "revision": "d39b8bb1c9a30bfbf8a9",
+    "url": "/epidemic/static/js/runtime~main.d39b8bb1.js"
+  },
+  {
+    "revision": "77493761a481118752eca6b6f540021b",
+    "url": "/epidemic/static/media/dif-easy.77493761.png"
+  },
+  {
+    "revision": "f76bd7ed63eec0e497eb1d7572afa594",
+    "url": "/epidemic/static/media/dif-medium.f76bd7ed.png"
+  },
+  {
+    "revision": "f94725da2c5f020f7fd21a6e2e112c24",
+    "url": "/epidemic/static/media/dif-hard.f94725da.png"
+  },
+  {
+    "revision": "0123b34457a16c865188ed7bcdc22f6b",
+    "url": "/epidemic/static/media/operations.0123b344.png"
+  },
+  {
+    "revision": "9cdc425c5f293afc85d4",
+    "url": "/epidemic/static/js/main.9cdc425c.chunk.js"
+  },
+  {
+    "revision": "05c95004ac27af298044cf07b68397e2",
+    "url": "/epidemic/static/media/medic.05c95004.png"
+  },
+  {
+    "revision": "01c38a931c4ccfce25a9bda45d820611",
+    "url": "/epidemic/static/media/quarantine.01c38a93.png"
+  },
+  {
+    "revision": "623b788ca84f21994f1e89c83c56108d",
+    "url": "/epidemic/static/media/researcher.623b788c.png"
+  },
+  {
+    "revision": "33aea064f59079ba2d6a1575fa5c821d",
+    "url": "/epidemic/static/media/scientist.33aea064.svg"
+  },
+  {
+    "revision": "ae1038f0d925e3468ac861528944a1f0",
+    "url": "/epidemic/static/media/dispatcher.ae1038f0.png"
+  },
+  {
+    "revision": "e4492b1a6ce3f198db226c7703d91a4f",
+    "url": "/epidemic/static/media/Simple_world_map.e4492b1a.svg"
+  },
+  {
+    "revision": "d5613da21f9b4b6f1f68d854e3572a90",
+    "url": "/epidemic/index.html"
+  }
+];
